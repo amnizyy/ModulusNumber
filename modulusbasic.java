7fc90c008a6398e1ada6d214379572bdd6795ecf
Izat Mohd <46371731+amnizyy@users.noreply.github.com>
2022-11-03 00:18:15 +0800

    Create modulusbasic.java

---
+++ modulusbasic.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        //modulus
+        //even
+        //aku pakat buat je ni haha tidur lu
+        int x = 10;
+        int num = x % 2;       
+        System.out.println("Modulus is " + num);
+    }
+}
